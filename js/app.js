906bf357f5eaba4ff83c32bcb1afd250952cdd69
Sergio Souza <emailsouza@gmail.com>
2018-06-28 11:35:59 -0600

    make babies

--- js/app.js
+++ js/app.js
@@ -29,6 +29,8 @@
 $(() => { // main jquery function - process all jquery
 
   console.log('Tomagotchi');
+  const petList = [];
+  const familyList = [];
   class Tomagotchi {
     constructor(name, age, image, hunger, sleepiness, boredom) {
       this.name = name;
@@ -37,122 +39,145 @@ $(() => { // main jquery function - process all jquery
       this.hunger = hunger;
       this.sleepiness = sleepiness;
       this.boredom = boredom;
+      petList.push(this);
     }
-
-    // setName() {
-    //   const $inputVal = $('input').val();
-    //   const $nameButton = $('<button/>');
-    //   $nameButton.text('Set Name');
-    //   $('body').append($nameButton);
-    //   $nameButton.on('click', () => {
-    //     this.name = $inputVal;
-    //     console.log(this.name);
-    //   })
-
-    //   this.name = name;
-    // }
-
     feedPet() {
       this.hunger -= 1;
     }
-
     playWithPet() {
       this.boredom -= 1;
     }
-
     lightsOff() {
       this.sleepiness -= 1;
     }
-
   } // End of Class
 
-  
-  const displayStats = function (tomagotchi) {
-    $('.stats').text(`Hunger: ${tomagotchi.hunger}\nSleepiness: ${tomagotchi.sleepiness}\nBoredom: ${tomagotchi.boredom}`);
+  // class Family {
+  //   constructor(name, age, image, hunger, sleepiness, boredom) {
+  //     this.name = name;
+  //     this.age = age;
+  //     this.image = image;
+  //     this.hunger = hunger;
+  //     this.sleepiness = sleepiness;
+  //     this.boredom = boredom;
+  //     petList.push(this);
+  //   }
+  //   feedPet() {
+  //     this.hunger -= 1;
+  //   }
+  //   playWithPet() {
+  //     this.boredom -= 1;
+  //   }
+  //   lightsOff() {
+  //     this.sleepiness -= 1;
+  //   }
+  // } // End of Class
 
+  const displayStats = function (tomagotchi) {
+    $($statsDiv[tomagotchi.name]).text(`Hunger: ${tomagotchi.hunger} Boredom: ${tomagotchi.boredom} Sleepiness: ${tomagotchi.sleepiness}`);
+    // $('span').text(`Hunger: ${tomagotchi.hunger} Boredom: ${tomagotchi.boredom} Sleepiness: ${tomagotchi.sleepiness}`);
   }
+
   const feed = function (tomagotchi) {
     const $feed = $('<button/>');
     $feed.text('Feed');
-    $($actionDiv).append($feed);
+    $feed.addClass('btn btn - primary');
+    $($actionDiv[tomagotchi.name]).append($feed);
     $feed.on('click', (e) => {
       tomagotchi.feedPet();
       displayStats(tomagotchi);
-      console.log(`Hunger is now : ${tomagotchi.hunger}`);
     })
   }
  
   const play = function (tomagotchi) {
     const $play = $('<button/>');
     $play.text('Play');
-    $($actionDiv).append($play);
+    $play.addClass('btn btn - primary');
+    $($actionDiv[tomagotchi.name]).append($play);
     $play.on('click', (e) => {
       tomagotchi.playWithPet();
       displayStats(tomagotchi);
-      console.log(`Boredom is now : ${tomagotchi.boredom}`);
     })
   }
 
   const lights = function (tomagotchi) {
     const $lights = $('<button/>');
     $lights.text('Lights');
-    $($actionDiv).append($lights);
+    $lights.addClass('btn btn - primary');
+    $($actionDiv[tomagotchi.name]).append($lights);
     $lights.on('click', (e) => {
       tomagotchi.lightsOff();
       displayStats(tomagotchi);
-        console.log(`Sleepines is now : ${tomagotchi.sleepiness}`);
-
     })
   }
  
-  const createButtons = function () {
-    feed(pet);
-    play(pet);
-    lights(pet);
+  const createButtons = function (tomagotchi) {
+    feed(tomagotchi);
+    play(tomagotchi);
+    lights(tomagotchi);
   }
 
+  const pet = new Tomagotchi('sushi', 5, "images/01-sushi-solo.jpeg", 1, 1, 1);
+  const pet2 = new Tomagotchi('sashimi', 5, "images/04-sushi.jpeg", 1, 2, 3);
+  // const pet4 = new Tomagotchi('beachy', 5, "images/03-sushi.jpg", 1, 2, 3);
 
-  // sushi ages very fast. must be eaten (disappear), or they mold
-  const pet = new Tomagotchi('sushi', 5, "images/01-sushi-solo.jpeg", 7, 2, 3);
-  const pet2 = new Tomagotchi('sushi', 5, "images/01-sushi-solo.jpeg", 7, 2, 3);
+  // const dad = new Family('sushi', 5, "images/01-sushi-solo.jpeg", 6, 2, 3);
+  // const mom = new Family('sashimi', 5, "images/04-sushi.jpeg", 1, 2, 3);
+  // const kid = new Family('beachy', 5, "images/03-sushi.jpg", 1, 2, 3);
 
+  const addBabyToPetList = function () {
+    const pet3 = new Tomagotchi('gumbino', 5, "images/02-sushi.jpg", 1, 2, 3);
+    const pet4 = new Tomagotchi('gumbina', 5, "images/02-sushi.jpg", 1, 2, 3);
+    console.log('called addBabyToPetList');
+  }
+  
   // GLOBAL Variables
-  // Create Divs, elements
-  const $tomaDiv = $('<div/>');
-  const $imageDiv = $('<div/>');
-  const $statsDiv = $('<div/> ');
-  const $actionDiv = $('<div/>');
-  const $image = $('<img/>');
-  $tomaDiv.addClass('tomagotchi-div');
-  $imageDiv.addClass('image-div');
-  $statsDiv.addClass('stats-div');
-  $actionDiv.addClass('actions-div'); 
-  $image.attr('src', pet.image);
+  let $tomaDiv = $('<div/>');
+  let $imageDiv = $('<div/>');
+  let $statsDiv = $('<div/>');
+  let $actionDiv = $('<div/>');
+  let $image = $('<img/>');
+  let $babyButton = $('<button/>');
 
-  //  Add Elements to the page
-  $('body').append($tomaDiv);
-  $tomaDiv.append($imageDiv);  
-  $tomaDiv.append($statsDiv);
-  $tomaDiv.append($actionDiv);
-  $imageDiv.append($image);  
+  const constructDivs = function (element) {
+    // Create Divs, elements
+    $tomaDiv[element.name] = $('<div/>');
+    $imageDiv[element.name] = $('<div/>');
+    $statsDiv[element.name] = $('<div/>');
+    $actionDiv[element.name] = $('<div/>');
+    $image[element.name] = $('<img/>');
 
-  const $h5 = $('<h5/>');
-  const $span = $('<span/>');
-  $span.addClass('stats');
-  $span.text('');
-  $h5.append($span);
-  $statsDiv.append($h5);
+    // Add Classes, attributes
+    $tomaDiv[element.name].addClass(element.name + ' tomadiv');
+    $imageDiv[element.name].addClass('image-div-' + element.name + ' image-div');  // not sure we need this
+    $statsDiv[element.name].addClass('stats-div-' + element.name + ' stats-div');  // not sure we need this
+    $actionDiv[element.name].addClass('action-div-' + element.name + ' action-div'); // not sure we need this
+    $image[element.name].addClass('image-' + element.name + ' image');
+    $image[element.name].attr('src', element.image);
 
+    // Add Elements to the page
+    $('.main').append($tomaDiv[element.name]);
+    $($tomaDiv[element.name]).append($imageDiv[element.name]);
+    $($tomaDiv[element.name]).append($statsDiv[element.name]);
+    $($tomaDiv[element.name]).append($actionDiv[element.name]);
+    $($imageDiv[element.name]).append($image[element.name]);
 
+    const $h5 = $('<h5/>');
+    const $span = $('<span/>');
+    // $span.addClass(element.name);
+    $span.addClass('span-' + element.name);
+    $span.text('');
+    $h5.append($span);
+    $($statsDiv[element.name]).append($h5);
+  }
 
   //  Main Function 
-  const game = () => {
-    createButtons();
+  const game = (tomagotchi) => {
+    createButtons(tomagotchi);
     let seconds = 0;
     const timePasssing = (tomagotchi) => {
-      
       seconds++;
-      if (seconds % 1 == 0) {
+      if (seconds % 3  == 0) {
         tomagotchi.hunger++;
       }
       if (seconds % 5 == 0) {
@@ -161,39 +186,70 @@ $(() => { // main jquery function - process all jquery
       if (seconds % 7 == 0) {
         tomagotchi.sleepiness++;
       }
-      if (seconds % 9 == 0) {
+      if (seconds % 3 == 0) {
         tomagotchi.age++;
+        $('.baby-button').append($babyButton);
       }
       // ...
       displayStats(tomagotchi);
+      
       if (tomagotchi.hunger >= 10 || tomagotchi.boredom >= 10 || tomagotchi.sleepiness >= 10) {
         console.log('Dead');
 
         clearInterval(timePasses);
         // Death stuff here
-        $actionDiv.hide();
-      }
+        tomagotchi.image = "images/rotten-fish-2.jpeg";  // assign new image to the object
+        $image[tomagotchi.name].attr('src', tomagotchi.image); // inserts image into page
+        const name = tomagotchi.name.toUpperCase();
+        $statsDiv[tomagotchi.name].text(`${name} is DEAD!`);
+        $actionDiv[tomagotchi.name].hide();
+      }      
     }
-    const timePasses = setInterval(timePasssing, 1000, pet);
+    const timePasses = setInterval(timePasssing, 1000, tomagotchi);
+  }
+
+  const makeBaby = function () {
+    // Using global variable -> $babyButton = $('<button/>');
+    $babyButton.addClass('start btn btn-primary');
+    $babyButton.text('Make Babies');
+    // $('.baby-button').append($babyButton);
+    $babyButton.on('click', (e) => {
+      addBabyToPetList();
+      //  $tomaDiv.empty();
+      $('.tomadiv').remove();
+      $babyButton.hide();
+      //restart game - TODO - this is not DRY (start function)
+      petList.forEach(tomagotchi => {
+        constructDivs(tomagotchi);
+        tomagotchi.sleepiness = 0;
+        game(tomagotchi);
+      });
+    });
+  };
+
+  const reset = function () {
 
   }
 
   // creates a start button, calls main game function
   const start = function (tomagotchi) {
     const $start = $('<button/>');
-    $start.addClass('start');
+    $start.addClass('start btn btn-primary');
     $start.text('Start');
     // $($actionDiv).append($start);
-    $('body').append($start);
+    $('.start-button').append($start);
     $start.on('click', (e) => {
-      game();
+      petList.forEach(tomagotchi => {
+        constructDivs(tomagotchi);
+        
+        game(tomagotchi);
+      });
       $start.hide(); 
-    })
-  }
+    });
+    makeBaby(); 
+  };
 
-  // s
-  start(pet);
-
+  start();
 
 });  // End of jquery main function - process all jquery
 
